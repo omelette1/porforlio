54022d3eda842e507d740928c54f42bc5700ad6d
omelette1 <stale1735@gmail.com>
2024-04-06 23:22:45 -0700

    update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static(buildPath));
 
 
 const corsOptions={
-  origin:'http://localhost:3001',
+  origin:'https://porforlio-n6rp.onrender.com',
   optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
